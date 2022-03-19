28e28fc51b2d026d4937657b4d7b1e87e6cca00e
sahar <s.behbahani71@outlook.com>
2022-03-19 20:55:37 +0330

    add home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
+import { Home } from "./components/home";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
